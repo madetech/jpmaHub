a1b9ab6cfb78478d7e6fcbda82c31f0d6e1fb275
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-01-11 17:04:42 +0000

    Add tgifs gateway

---
+++ lib/gateway/tgifs_gateway.rb
@@ -0,0 +1,45 @@
+require 'sinatra/activerecord'
+
+module Gateway
+  class TgifsGateway
+    class Tgif < ActiveRecord::Base
+
+    end
+
+    def fetch_tgif
+      weekly_time_range = (6.days.ago..DateTime.now)
+      tgifs = Tgif.where(:created_at => weekly_time_range)
+
+      return [] if tgifs.empty?
+
+      tgifs.map do |tgif_details|
+        tgif_builder = Builder::Tgif.new
+        tgif_builder.from(
+            id: tgif_details[:id],
+            team_name: tgif_details[:team_name],
+            message: tgif_details[:message]
+        )
+        tgif_builder.build
+      end
+    end
+
+    def save(tgif)
+      serialised_tgif = {
+          team_name: tgif.team_name,
+          message: tgif.message,
+          created_at: DateTime.now,
+          slack_user_id: tgif.slack_user_id,
+      }
+
+      Tgif.create(serialised_tgif)
+    end
+
+    def delete_all
+      Tgif.delete_all
+    end
+
+    def delete_tgif(team_name)
+      Tgif.delete_by('lower(team_name) = ?', team_name.downcase)
+    end
+  end
+end
--- spec/unit/tgifs_gateway_spec.rb
+++ spec/unit/tgifs_gateway_spec.rb
@@ -1,38 +1,15 @@
-xdescribe SequelTgifGateway do
+describe Gateway::TgifsGateway do
 
-  let(:sequel_tgif_gateway) { described_class.new(database: database) }
-
-  before do
-    sequel_tgif_gateway.delete_all
-  end
-
-  it 'can get no tgifs' do
-    expect(sequel_tgif_gateway.all).to eq([])
-  end
-
-  it 'can get a list of tgifs' do
-    tgif_builder = Builder::Tgif.new
-    tgif_builder.from(team_name: 'TeamOne', message: 'team-one-message')
-
-    tgifs = tgif_builder.build
-
-    sequel_tgif_gateway.save(tgifs)
-
-    sequel_tgif_gateway.all.first.tap do |tgif|
-      expect(tgif.id).not_to be nil
-      expect(tgif.team_name).to eq('TeamOne')
-      expect(tgif.message).to eq('team-one-message')
-    end
-  end
-
+  let(:tgif_gateway) { described_class.new }
+  
   it 'can get a weekly list of tgifs' do
     populate_weekly_tgif('team_name_one', 'message_one', DateTime.now - 8)
     populate_weekly_tgif('team_name_two', 'message_two', DateTime.now + 1)
     populate_weekly_tgif('team_current_week', 'message', DateTime.now + 7)
 
-    expect(sequel_tgif_gateway.fetch_tgif.count).to eq(1)
+    expect(tgif_gateway.fetch_tgif.count).to eq(1)
 
-    sequel_tgif_gateway.fetch_tgif.first.tap do |tgif|
+    tgif_gateway.fetch_tgif.first.tap do |tgif|
       expect(tgif.id).not_to be nil
       expect(tgif.team_name).to eq('team_current_week')
       expect(tgif.message).to eq('message')
@@ -44,36 +21,34 @@ xdescribe SequelTgifGateway do
     populate_tgif('team_name_two', 'message_two')
     populate_tgif('team_current_week', 'message')
 
-    expect(sequel_tgif_gateway.all.count).to eq(3);
+    expect(tgif_gateway.fetch_tgif.count).to eq(3);
 
-    sequel_tgif_gateway.delete_all
+    tgif_gateway.delete_all
 
-    expect(sequel_tgif_gateway.all.count).to eq(0);
+    expect(tgif_gateway.fetch_tgif.count).to eq(0);
   end
 
   it 'can delete tgif by team' do
     populate_tgif('team_name_two', 'message_two')
     populate_tgif('team', 'message_one')
 
-    expect(sequel_tgif_gateway.all.count).to eq(2);
+    expect(tgif_gateway.fetch_tgif.count).to eq(2);
 
-    sequel_tgif_gateway.delete_tgif('Team')
+    tgif_gateway.delete_tgif('Team')
 
-    expect(sequel_tgif_gateway.all.count).to eq(1);
+    expect(tgif_gateway.fetch_tgif.count).to eq(1);
   end
 end
 
 def populate_weekly_tgif(team_name, message, time = nil)
   tgif_builder = Builder::Tgif.new
   tgif_builder.from(team_name: team_name, message: message)
-  tgifs = tgif_builder.build
   allow(DateTime).to receive(:now).and_return(time)
-  sequel_tgif_gateway.save(tgifs)
+  tgif_gateway.save(tgif_builder.build)
 end
 
 def populate_tgif(team_name, message)
   tgif_builder = Builder::Tgif.new
   tgif_builder.from(team_name: team_name, message: message)
-  tgifs = tgif_builder.build
-  sequel_tgif_gateway.save(tgifs)
+  tgif_gateway.save(tgif_builder.build)
 end 
