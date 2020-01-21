eff5dcef41c61b487578b7bfc206460d04174f78
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-01-21 16:13:31 +0000

    Add focus to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,7 @@ RSpec.configure do |config|
   config.include RSpecMixin
   config.include Rack::Test::Methods
   config.run_all_when_everything_filtered
+  config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
